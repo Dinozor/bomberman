ec3ec473943d66968c9ea930425620fe5346d334
Dino <dinocrys@gmail.com>
2025-11-25 22:08:45 +0200

    Fix scene cell removal

--- core/stupid_tilemap.gd
+++ core/stupid_tilemap.gd
@@ -6,6 +6,7 @@ var _scene_instances: Dictionary[Vector2i, Node2D]
 
 func _ready():
 	child_entered_tree.connect(_on_child_entered_tree)
+	child_exiting_tree.connect(_on_child_exited_tree)
 
 
 func get_scene_at_cell(coords: Vector2i) -> Node2D:
@@ -17,3 +18,9 @@ func get_scene_at_cell(coords: Vector2i) -> Node2D:
 func _on_child_entered_tree(child: Node) -> void:
 	var cell_coords: Vector2i = local_to_map(child.position)
 	_scene_instances[cell_coords] = child as Node2D
+
+
+func _on_child_exited_tree(child: Node) -> void:
+	var cell_coords: Vector2i = local_to_map(child.position)
+	_scene_instances.erase(cell_coords)
+	erase_cell(cell_coords)
